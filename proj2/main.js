876b94037a85879a5f25cca95a55fe976fb14879
Rahul jain <rahul.jain@w3villa.com>
2022-11-15 10:40:33 +0530

    change Color mini Project

--- proj2/main.js
+++ proj2/main.js
@@ -7,14 +7,58 @@ const pink = document.querySelector(".pink");
 
 const center = document.querySelector(".center");
 
-// console.log(window.getComputedStyle(red).backgroundColor);
+
+// window.getComputedStyle
+
+console.log(window.getComputedStyle(red).backgroundColor);
+
+const getBGColor = (selectedElement) => {
+  return window.getComputedStyle(selectedElement).backgroundColor
+}
+var color = getBGColor(orange);
 
 
-const setBg = () => {
-    const randomColor = Math.floor(Math.random()*16777215).toString(16);
-    document.body.style.backgroundColor = "#" + randomColor;
-    color.innerHTML = "#" + randomColor;
-  }
+
+
+const magicColorChanger = (element, color) =>{
+  element.addEventListener('mouseenter', ()=>{
+    center.style.background =  color; 
+  } )
+}
+
+
+magicColorChanger(red, getBGColor(red));
+magicColorChanger(cyan, getBGColor(cyan));
+magicColorChanger(violet, getBGColor(violet));
+magicColorChanger(orange, getBGColor(orange));
+magicColorChanger(pink, getBGColor(pink));
+
+
+// const setBg = () => {
+//     const randomColor = Math.floor(Math.random()*16777215).toString(16);
+//     document.body.style.backgroundColor = "#" + randomColor;
+//     color.innerHTML = "#" + randomColor;
+//   }
   
-  genNew.addEventListener("click", setBg);
-  setBg();
+//   genNew.addEventListener("click", setBg);
+//   setBg();
+
+
+
+// var counter = document.querySelector(".counter");
+// var followers = document.querySelector(".followers");
+
+// let count = 1;
+
+// setInterval(()=>{
+//   if(count < 1000){
+
+//     count++;
+//     counter.innerText = count;
+//   }
+// },1)
+
+// setTimeout(()=>{
+//   followers.innerText = "Followers on Instagram!"
+// },3000)
+
